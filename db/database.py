f2aed24bb84ee6f8f3a8d9325648d1b312168ee7
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-03-13 16:39:42 +0300

    create db

---
+++ db/database.py
@@ -0,0 +1,40 @@
+import aiosqlite
+
+DB_PATH = "users.db"
+
+
+async def create_db():
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,    
+                id_tg INTEGER DEFAULT 0,
+                username TEXT,
+                points INTEGER DEFAULT 0
+            )
+        """)
+        await db.commit()
+
+
+async def register_user(user_id, username):
+    async with aiosqlite.connect(DB_PATH) as db:
+        async with db.execute(
+            "SELECT username FROM users WHERE id_tg = ?", (user_id,)
+        ) as cursor:
+            row = await cursor.fetchone()
+            if not row:
+                await db.execute(
+                    """
+                    INSERT INTO users (id_tg, username) 
+                    VALUES (?, ?)
+                    """,
+                    (user_id, username),
+                )
+        await db.commit()
+
+
+async def get_all_users():
+    async with aiosqlite.connect(DB_PATH) as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            users = await cursor.fetchall()
+            return users
