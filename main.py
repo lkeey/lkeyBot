dcb8b56ce95e2f79c8d45e433464b8faf16aff22
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-06-25 23:39:22 +0300

    update static

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from telegram.ext import (
     MessageHandler,
     filters,
     ConversationHandler,
+    CallbackQueryHandler,
 )
-from telegram import (
-    Update,
-)
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 
-from states import GET_QUESTION, GET_QUESTION_FOR_POLL
 
 from db.database import create_db, register_user, get_all_users
 
-import asyncio, json
+from static.text import GREETING, ABOUT_INTENSIV, DESCRIPTION_1, DESCRIPTION_2
+
+import asyncio
 
 DB_PATH = "users.db"
 
@@ -30,106 +30,168 @@ load_dotenv()
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
 
+    keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🌐 Об интенсиве", callback_data="about_course")]]
+    )
+
     async with aiosqlite.connect(DB_PATH) as db:
         async with db.execute(
             "SELECT username FROM users WHERE id_tg = ?", (user_id,)
         ) as cursor:
             row = await cursor.fetchone()
-            if row:
-                await context.bot.send_message(
-                    chat_id=update.effective_chat.id,
-                    text="С возвращением!",
-                )
-            else:
+            if not row:
                 await register_user(
                     user_id=user_id,
                     username=update.effective_user.username,
                 )
-                await context.bot.send_message(
-                    chat_id=update.effective_chat.id,
-                    text="Вы успещно зарегистрированы",
+
+    with open("img/7M307113.JPG", "rb") as f:
+        await context.bot.send_photo(
+            chat_id=update.effective_chat.id,
+            photo=f,
+            caption=GREETING,
+            parse_mode="Markdown",
+            reply_markup=keyboard,
+        )
+
+
+async def about_course_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+
+    await query.answer()
+
+    keyboard = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("🔹 Базовый курс", callback_data="basic_course")],
+            [
+                InlineKeyboardButton(
+                    "🔸 Продвинутый курс", callback_data="advanced_course"
                 )
-    return 0
-
-
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="hello",
+            ],
+        ]
     )
 
-    users = await get_all_users()
-
-    print(users)
-
-
-async def get_question(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Введите ваш вопрос:",
-    )
-    return GET_QUESTION
+    with open("img/course_1.jpg", "rb") as photo:
+        await context.bot.send_photo(
+            chat_id=update.effective_chat.id,
+            photo=photo,
+            caption=ABOUT_INTENSIV,
+            parse_mode="Markdown",
+            reply_markup=keyboard,
+        )
 
 
-async def save_question(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=GROUP_ID,
-        text=update.message.text,
-    )
+async def intensiv_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
 
-
-async def create_poll(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=GROUP_ID,
-        text="Создание опроса!\n\nВведите вопрос",
+    keyboard = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("🔹 Базовый курс", callback_data="basic_course")],
+            [
+                InlineKeyboardButton(
+                    "🔸 Продвинутый курс", callback_data="advanced_course"
+                )
+            ],
+        ]
     )
 
-    return GET_QUESTION_FOR_POLL
+    with open("img/course_1.jpg", "rb") as photo:
+        await context.bot.send_photo(
+            chat_id=user_id,
+            photo=photo,
+            caption=ABOUT_INTENSIV,
+            parse_mode="Markdown",
+            reply_markup=keyboard,
+        )
 
 
-async def create_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    with open("polls.json", "r") as f:
-        polls = json.load(f)
+async def course_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
 
-    polls[max(polls.keys()) + 1] = {"question": update.message.text}
+    chat_id = update.effective_chat.id
+    bot = context.bot
 
-    with open("polls.json", "w") as f:
-        json.dump(polls, f)
+    if query.data == "basic_course":
+        with open("img/course_2.jpg", "rb") as photo:
+            keyboard = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "📝 Записаться", url="https://t.me/m/boXhH69MYzRi"
+                        )
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "⚡️ Подробнее", url="http://by.lkey.tilda.ws/base"
+                        )
+                    ],
+                ]
+            )
 
-    await context.bot.send_message(
-        chat_id=GROUP_ID,
-        text="Создание опроса!\n\nВведите опции",
+            await context.bot.send_photo(
+                chat_id=update.effective_chat.id,
+                photo=photo,
+                caption=DESCRIPTION_1,
+                parse_mode="Markdown",
+                reply_markup=keyboard,
+            )
+
+    elif query.data == "advanced_course":
+        with open("img/course_3.jpg", "rb") as photo:
+            keyboard = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "📝 Записаться", url="https://t.me/m/jWlVlaBRMWZi"
+                        )
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "⚡️ Подробнее", url="http://by.lkey.tilda.ws/super"
+                        )
+                    ],
+                ]
+            )
+
+            await context.bot.send_photo(
+                chat_id=update.effective_chat.id,
+                photo=photo,
+                caption=DESCRIPTION_2,
+                parse_mode="Markdown",
+                reply_markup=keyboard,
+            )
+
+    asyncio.create_task(send_follow_up(chat_id, bot))
+
+
+async def send_follow_up(chat_id, bot):
+    await asyncio.sleep(900)
+    await bot.send_message(
+        chat_id=chat_id,
+        text="❓ Ещё не готов купить?\n\nПосмотри более подробную информацию на нашем сайте:\nhttp://by.lkey.tilda.ws/",
     )
-
-    print()
-
-    return 0
 
 
 def main():
     print("MAIN")
 
-    TOKEN = os.getenv("TOKEN")
+    # TOKEN = os.getenv("TOKEN")
+    TOKEN = "8025550158:AAEVGc6fHWZb6e1Xlh3DNXpYgfhoxi3jP48"
 
     application = Application.builder().token(TOKEN).build()
 
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-        states={
-            GET_QUESTION: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, save_question)
-            ],
-            GET_QUESTION_FOR_POLL: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, create_task)
-            ],
-        },
-        fallbacks=[
-            CommandHandler("question", get_question),
-            CommandHandler("createpoll", create_poll),
-        ],
-        persistent=False,
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("info", intensiv_info))
+
+    application.add_handler(
+        CallbackQueryHandler(about_course_callback, pattern="about_course")
     )
-    application.add_handler(conv_handler)
+    application.add_handler(
+        CallbackQueryHandler(
+            course_callback, pattern="^(basic_course|advanced_course)$"
+        )
+    )
 
     application.run_polling()
 
--- test.py
+++
@@ -1,6 +0,0 @@
-from json import *
-
-question = {1: {"task": "Сколько будет 2+2?", "options": ["3", "4", "5", "6"]}}
-
-with open("polls.json", "w") as f:
-    dump(question, f)
